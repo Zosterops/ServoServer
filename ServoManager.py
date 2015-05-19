84ce7fa267a36fd65e0a5ad387c9f5a887ad4045
thetlk <jeremieboutoille@gmail.com>
2015-05-19 15:26:49 +0000

    Now able to move servo with angle

--- ServoManager.py
+++ ServoManager.py
@@ -1,20 +1,28 @@
 
+import logging
+from RPIO import PWM
+
 class Servo:
     """
     Represent a servo motor
     """
 
     def __init__(self, gpio):
+        self.logger = logging.getLogger('Servo')
+        self.logger.debug('__init__')
+        self.servo = PWM.Servo(pulse_incr_us=1)
         self.gpio = gpio
         self.frequency = 50 # 50hz frequency
-        self.angle_0 = 5 # 0.5 ms
-        self.angle_180 = 25 # 2.5ms
+        self.angle_0 = 250 # 250 us
+        self.angle_180 = 2300 # 2300us
 
     def get_duty_cycle(self, angle):
-        return 5
+        return (angle * self.angle_180) / 180
 
     def move(self, angle):
-        pass
+        duty_cycle = self.get_duty_cycle(angle)
+        self.logger.debug('Servo %d : angle : %d, duty_cycle : %d' % (self.gpio, angle, duty_cycle))
+        self.servo.set_servo(self.gpio, duty_cycle)
 
 
 class ServoManager:
@@ -42,3 +50,9 @@ class ServoManager:
     def move_right_lef(self, angle):
         self.servo_right_left.move(angle)
 
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, format='%(name)s: %(message)s')
+    servo = Servo(17)
+    while True:
+        servo.move(input())
